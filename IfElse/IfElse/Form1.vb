0b51bf0d90378126f69564b7316adde2d4ea9d99
idsc3101 <idsc3101@gmail.com>
2013-09-23 15:02:35 -0500

    UPDATED

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -6,8 +6,21 @@ Public Class Form1
 
         Dim num1, num2, largerNum As Double
 
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+
+            MessageBox.Show("Error: Please Type A Number")
+            Return
+
+        End If
+
         num1 = CDbl(txtFirstNum.Text)
         num2 = CDbl(txtSecondNum.Text)
+
+        If (num1 < 0) Or (num2 < 0) Then
+            MessageBox.Show("Error: Please Type A Positive Number")
+            Return
+
+        End If
 
         If num1 > num2 Then
             largerNum = num1
